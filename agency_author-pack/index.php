31b4938e3e5d184f2757ea5dc97f68050eb5a97f
Adam <amsunderman@gmail.com>
2014-04-10 08:40:42 -0500

    working site

--- agency_author-pack/index.php
+++ agency_author-pack/index.php
@@ -8,39 +8,31 @@ License URL: http://creativecommons.org/licenses/by/3.0/
  *
  */
 include "app/config.php";
-include "app/detect.php";
 
 if ($page_name=='') {
-	include $browser_t.'/index.html';
+	include "web/index.html";
 	}
 elseif ($page_name=='index.html') {
-	include $browser_t.'/index.html';
+	include "web/index.html";
 	}
-elseif ($page_name=='about.html') {
-	include $browser_t.'/about.html';
+elseif ($page_name=='reflections.html') {
+	include "web/reflections.html";
 	}
-elseif ($page_name=='bloginner.html') {
-	include $browser_t.'/bloginner.html';
+elseif ($page_name=='template.html') {
+	include "web/template.html";
 	}
-elseif ($page_name=='blogsingle.html') {
-	include $browser_t.'/blogsingle.html';
+elseif ($page_name=='seniordesign.html') {
+	include "web/seniordesign.html";
 	}
-elseif ($page_name=='icons.html') {
-	include $browser_t.'/icons.html';
+elseif ($page_name=='autocad_revisioncontrol.html') {
+	include "web/autocad_revisioncontrol.html";
 	}
-elseif ($page_name=='iconsingle.html') {
-	include $browser_t.'/iconsingle.html';
-	}
-elseif ($page_name=='contact.html') {
-	include $browser_t.'/contact.html';
-	}
-elseif ($page_name=='contact-post.html') {
-	include $browser_t.'/contact.html';
-	include 'app/contact.php';
+elseif ($page_name=='memegenerator.html') {
+	include "web/memegenerator.html";
 	}
 else
 	{
-		include $browser_t.'/404.html';
+		include "web/404page.html";
 	}
 
 ?>
